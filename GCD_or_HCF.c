a292c666a9f97c792606de1b08ecff1b7336899c
harshi-1201 <120094201+harshi-1201@users.noreply.github.com>
2023-02-05 09:50:44 +0530

    #include<stdio.h> int main() {     int n,m,g,max,i;     scanf("%d %d",&n,&m);     g=(n>m)?n:m;     for(i=1;i<=g;i++)     if(n%i==0 && m%i==0)     max=i;     printf("%d",max); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,g,max,i;
+    scanf("%d %d",&n,&m);
+    g=(n>m)?n:m;
+    for(i=1;i<=g;i++)
+    if(n%i==0 && m%i==0)
+    max=i;
+    printf("%d",max);
+}
